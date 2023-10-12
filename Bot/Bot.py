b05f609de08a0e1e8564a88de6b6ae777d1fc14e
ITJEONG-DEV <derbana1027@gmail.com>
2023-10-12 13:49:03 +0900

    client.create_tweet 리턴값 확인 2

--- Bot/Bot.py
+++ Bot/Bot.py
@@ -100,8 +100,7 @@ class TwitterBot:
             in_reply_to_tweet_id=reply_id
         )
 
-        print(response.json())
-
+        print(response.data)
         print(f"다음의 내용을 트윗합니다.\n{message}, {media.media_id}")
 
-        return response.json()
+        return response.data
