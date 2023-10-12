37375dfacea926e1b1dd6875c09579aff034745d
ITJEONG-DEV <derbana1027@gmail.com>
2023-10-12 13:10:26 +0900

    참조 수정 2

--- Bot/Bot.py
+++ Bot/Bot.py
@@ -6,7 +6,7 @@ import os
 import tweepy
 
 from util import read_json
-from lostark.api.adventure_island import get_adventure_island
+from lostark.api import get_adv enture_island
 
 
 class TwitterBot:
